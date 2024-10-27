331c6fde56d95fb89d86834d8f13d7c556d7f582
Culaa <faisalfirdaus1125@gmail.com>
2024-10-28 00:29:37 +0700

    Keding Wedding Organizer

---
+++ src/js/services.js
@@ -0,0 +1,168 @@
+/* MODAL WEDDING 1*/
+const modal_overlay = document.querySelector('#modal_overlay');
+const modal = document.querySelector('#modal');
+
+function openModal(value){
+    if(value){
+        modal_overlay.classList.remove('hidden')
+        modal_overlay.classList.add('flex')
+        setTimeout(() => {
+            modal.classList.remove('opacity-0')
+        }, 100);
+    }
+    else{
+        modal.classList.add('opacity-0')
+        setTimeout(() => {
+            modal_overlay.classList.add('hidden')
+            modal_overlay.classList.remove('flex')
+            }, 300);
+    }
+}
+
+/* MODAL WEDDING 2*/
+const modal_overlay2 = document.querySelector('#modal_overlay2');
+const modal2 = document.querySelector('#modal2');
+
+function openModal2(value){
+    if(value){
+        modal_overlay2.classList.remove('hidden')
+        modal_overlay2.classList.add('flex')
+        setTimeout(() => {
+            modal2.classList.remove('opacity-0')
+        }, 100);
+    }
+    else{
+        modal2.classList.add('opacity-0')
+        setTimeout(() => {
+            modal_overlay2.classList.add('hidden')
+            modal_overlay2.classList.remove('flex')
+            }, 300);
+    }
+}
+
+/* MODAL WEDDING 3*/
+const modal_overlay3 = document.querySelector('#modal_overlay3');
+const modal3 = document.querySelector('#modal3');
+
+function openModal3(value){
+    if(value){
+        modal_overlay3.classList.remove('hidden')
+        modal_overlay3.classList.add('flex')
+        setTimeout(() => {
+            modal3.classList.remove('opacity-0')
+        }, 100);
+    }
+    else{
+        modal3.classList.add('opacity-0')
+        setTimeout(() => {
+            modal_overlay3.classList.add('hidden')
+            modal_overlay3.classList.remove('flex')
+            }, 300);
+    }
+}
+
+/* MODAL WEDDING 4*/
+const modal_overlay4 = document.querySelector('#modal_overlay4');
+const modal4 = document.querySelector('#modal4');
+
+function openModal4(value){
+    if(value){
+        modal_overlay4.classList.remove('hidden')
+        modal_overlay4.classList.add('flex')
+        setTimeout(() => {
+            modal4.classList.remove('opacity-0')
+        }, 100);
+    }
+    else{
+        modal4.classList.add('opacity-0')
+        setTimeout(() => {
+            modal_overlay4.classList.add('hidden')
+            modal_overlay4.classList.remove('flex')
+            }, 300);
+    }
+}
+
+
+/* MODAL PHOTO 1*/
+const modal_overlay_photo = document.querySelector('#modal_overlay_photo');
+const modal_photo = document.querySelector('#modal_photo');
+
+function openphotoModal(value){
+    if(value){
+        modal_overlay_photo.classList.remove('hidden')
+        modal_overlay_photo.classList.add('flex')
+        setTimeout(() => {
+            modal_photo.classList.remove('opacity-0')
+        }, 100);
+    }
+    else{
+        modal_photo.classList.add('opacity-0')
+        setTimeout(() => {
+            modal_overlay_photo.classList.add('hidden')
+            modal_overlay_photo.classList.remove('flex')
+            }, 300);
+    }
+}
+
+/* MODAL PHOTO 2*/
+const modal_overlay_photo2 = document.querySelector('#modal_overlay_photo2');
+const modal_photo2 = document.querySelector('#modal_photo2');
+
+function openphotoModal2(value){
+    if(value){
+        modal_overlay_photo2.classList.remove('hidden')
+        modal_overlay_photo2.classList.add('flex')
+        setTimeout(() => {
+            modal_photo2.classList.remove('opacity-0')
+        }, 100);
+    }
+    else{
+        modal_photo2.classList.add('opacity-0')
+        setTimeout(() => {
+            modal_overlay_photo2.classList.add('hidden')
+            modal_overlay_photo2.classList.remove('flex')
+            }, 300);
+    }
+}
+
+/* MODAL PHOTO 3*/
+const modal_overlay_photo3 = document.querySelector('#modal_overlay_photo3');
+const modal_photo3 = document.querySelector('#modal_photo3');
+
+function openphotoModal3(value){
+    if(value){
+        modal_overlay_photo3.classList.remove('hidden')
+        modal_overlay_photo3.classList.add('flex')
+        setTimeout(() => {
+            modal_photo3.classList.remove('opacity-0')
+        }, 100);
+    }
+    else{
+        modal_photo3.classList.add('opacity-0')
+        setTimeout(() => {
+            modal_overlay_photo3.classList.add('hidden')
+            modal_overlay_photo3.classList.remove('flex')
+            }, 300);
+    }
+}
+
+/* MODAL PHOTO 4*/
+const modal_overlay_photo4 = document.querySelector('#modal_overlay_photo4');
+const modal_photo4 = document.querySelector('#modal_photo4');
+
+function openphotoModal4(value){
+    if(value){
+        modal_overlay_photo4.classList.remove('hidden')
+        modal_overlay_photo4.classList.add('flex')
+        setTimeout(() => {
+            modal_photo4.classList.remove('opacity-0')
+        }, 100);
+    }
+    else{
+        modal_photo4.classList.add('opacity-0')
+        setTimeout(() => {
+            modal_overlay_photo4.classList.add('hidden')
+            modal_overlay_photo4.classList.remove('flex')
+            }, 300);
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: ['Montserrat'],
+        greatvibe: ['Great Vibes']
+      }
+    },
+  },
+  plugins: [],
+}
+
